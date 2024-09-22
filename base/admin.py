79b018ae6a2896ed69fbcadadd8110a24b81c06e
UtsavlalShrestha <utsavlalshrestha@gmail.com>
2024-09-22 16:58:53 +0545

    models added

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from  .models import Hospital, Doctor, Patient
+
 
 # Register your models here.
+admin.site.register(Hospital)
+admin.site.register(Doctor)
+admin.site.register(Patient)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    speciality = models.CharField(max_length=100)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"Dr. {self.name}"    
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    doctors = models.ManyToManyField('Doctor', related_name='patients')
+
+    def __str__(self):
+        return self.name
